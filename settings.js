bc89fe4cd3fc45058108f20e983e3afcea8df5b5
Cybersmokeymass <104247504+Cybersmokeymass@users.noreply.github.com>
2022-06-05 22:09:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+918547584245']
+global.premium = ['+918547584245']
+global.ownernomer = '+918547584245'
+global.ownername = '🐦Nx.jd🦋'
+global.botname = 'sarasu'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://instagram.com/_.nx.jd._?igshid=YmMyMTA2M2Y'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.sc = 'https://github.com/Cybersmokeymass'
+global.myweb = ''
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '🐦Star Boy🦋'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
